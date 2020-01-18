60823a3e058e39a277457445d19d34be9f0c142e
weblidity <shytiger@yahoo.com>
2020-01-18 12:58:56 +0200

    Performed little changes

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-var docsog = require("./lib/docusaurusOutlineGenerator");
 
-module.eports.docsog = docsog;
+/*
+  Export docusaurus-outline-generator
+*/
+
+module.exports = require("./lib/docusaurusOutlineGenerator");
+
+/*
+  Export the version
+*/
+
+module.exports.version = require('./package.json').version;
