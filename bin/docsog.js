3e868f396d98e86e9b98ecf1ef8d122230101f0d
weblidity <shytiger@yahoo.com>
2020-01-21 00:08:03 +0200

    Got to a stablestage

--- bin/docsog.js
+++ bin/docsog.js
@@ -43,13 +43,29 @@ program
 
     });
 
+let defaultOptions = docsog.defaultOptions;
+
 program
    .command("generate [source]")
    .description("generate topic files in docs and navigation in sidebars.json")
-   .option("-d, --docspath <path>", "path where topic files are generated")
-   .action((source) => {
+   .option("-d, --docspath <path>", "path where documentation documents will be generated", defaultOptions.docsPath)
+   .option("-t, --templatespath <path>", "path to templates used during generation", defaultOptions.templatesPath)
+   .option("-w, --websitepath <path>", "path where sidebars.json will be generated", defaultOptions.websitePath)
+   .option("-c, --config <file>", "filename or full path to generation configuration file", "docsog-config.json")
+   .action((source, cmdObj) => {
+      console.log("Generate");
+      console.log(cmdObj.opts());
+      if (!source) {
+        source = "docsog-outline.json";
+      }
+      console.log("Will use " + source);
+      let outlineSource = fs.readFileSync(source, "utf8");
+      let outline = JSON.parse(outlineSource);
+      console.log(outline.topics);
 
+      docsog.generate(outline.topics, defaultOptions);
    })
+   
 
 program.parse(process.argv);
 
